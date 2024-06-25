d5572bb83be70d90c4feea1a77d4a5bf600d7803
IvanRomashovNew <ivanromashov2010@gmail.com>
2024-06-26 00:47:21 +0300

    V2 (удалил ненужный файл)

--- build/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-service postgresql start
-su - postgres -c '
-psql -c "CREATE USER postgresnew WITH PASSWORD '\''111'\'';"
-createdb -O postgresnew SeaRadarDB
-'
-xvfb-run java -jar app.jar
